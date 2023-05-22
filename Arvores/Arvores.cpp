27459385822a18429db91ccc0ee468ffd9309d6f
Fernando Gomes de Sousa Junior <fejunior5@hotmail.com>
2023-05-22 00:24:23 -0300

    Add print inOrder, posOrder and function countNode

--- Arvores/Arvores.cpp
+++ Arvores/Arvores.cpp
@@ -6,13 +6,55 @@
 int main()
 {
 	Tree<int> intTree;
-	int value;
+	int value, choice=1;
 	
+	cout << "#### JUNONN LAB TREE ####\n";
+	
+
+	while (choice != 0) {
+		intTree.printDefault();
+		cout<<"\n-----------------------\n";
+		cout << "#0 Sair\n#1 Inserir\n#2 Imprimir Pre-Ordem"
+			<< "\n#3 Imprimir em Ordem\n#4 Imprimir Pos-Ordem\n"
+			<< "\n#5 Quantidade de noh\n#6 Imprimir Pos-Ordem\n"<<
+			"-----------------------\n";
+
+		cout << "\nEscolha uma opcao: ";
+		cin >> choice;
+		system("cls");
+		
+		
+		switch (choice)
+		{
+			case 1:
+				cout << "#Digite um valor: ";
+				cin >> value;
+				intTree.insertNode(value);
+				break;
+			case 2:
+				cout << "Imprimindo pre-ordem\n";
+				intTree.printPreOrder();
+				break;
+			case 3:
+				cout << "Imprimindo ordem\n";
+				intTree.printInOrder();
+				break;
+			case 4:
+				cout << "Imprimindo pos-ordem\n";
+				intTree.printInOrder();
+				break;
+			case 5:
+				cout << "Exite(m) "<<intTree.countNode()<<" noh(s)\n\n\n";
+				break;
+
+			default:
+			break;
+		}
+	}
+
 	cout << "Digite 10 numeros para compor a arvore:\n";
 	for (int i = 0; i < 10; i++)
 	{//Le o valor e insere na arvore
-		cin >> value;
-		intTree.insertNode(value);
 	}
 
 	cout << "Imprimindo Arvore...\n";
--- Arvores/Tree.h
+++ Arvores/Tree.h
@@ -11,10 +11,18 @@ class Tree
 		Node<T>* root;
 		void insertNodeHelp(Node<T>**, const T&);//Auxiliar para inserir um noh
 		void printPreOrderHelp(Node<T>*)const;
+		void printInOrderHelp(Node<T>*)const;
+		void printPosOrderHelp(Node<T>*)const;
+		void printDefaultHelp(Node<T>*, int, bool)const;
+		int countNodeHelp(Node<T>*)const;
 	public:
 		Tree();
 		void insertNode(const T&);//Inserir um noh
 		void printPreOrder()const;
+		void printInOrder()const;
+		void printPosOrder()const;
+		void printDefault()const;
+		int countNode()const;
 
 };
 
@@ -53,6 +61,7 @@ void Tree<T>::printPreOrder()const {
 
 template <typename T>
 void Tree<T>::printPreOrderHelp(Node<T> *currentNode)const {
+	//Imprime em pre-ordem
 	if (currentNode != nullptr) {
 		cout <<"("<< currentNode->getData();
 		printPreOrderHelp(currentNode->nodeLeft);
@@ -60,8 +69,95 @@ void Tree<T>::printPreOrderHelp(Node<T> *currentNode)const {
 		cout << ")";
 	}
 	else {
-		cout << "()";
+		cout << "*";
 	}
+}
+
+
+template <typename T>
+void Tree<T>::printInOrder()const {
+	printInOrderHelp(root);
+}
+
+template <typename T>
+void Tree<T>::printInOrderHelp(Node<T> *currentNode)const {
+	//Imprime em ordem
+	if (currentNode != nullptr) {
+		cout << "(";
+		printInOrderHelp(currentNode->nodeLeft);
+		cout << currentNode->getData();
+		printInOrderHelp(currentNode->nodeRight);
+		cout << ")";
+	}
+	else {
+		cout << "*";
+	}
+}
+
+template <typename T>
+void Tree<T>::printPosOrder()const {
+	printPosOrderHelp(root);
+}
+
+template <typename T>
+void Tree<T>::printPosOrderHelp(Node<T> *currentNode)const {
+	//Imprime em pos-ordem
+	if (currentNode != nullptr) {
+		cout << "(";
+		printPosOrderHelp(currentNode->nodeLeft);
+		printPosOrderHelp(currentNode->nodeRight);
+		cout << currentNode->getData();
+		cout << ")";
+	}
+	else {
+		cout << "*";
+	}
+}
+
+template <typename T>
+void Tree<T>::printDefault()const {
+	printDefaultHelp(root, 0, false);
+}
+
+template <typename T>
+void Tree<T>::printDefaultHelp(Node<T> *currentNode, int level, bool left)const {
+	//Imprime em arvore na horizontal
+	
+	if (currentNode == NULL) {
+		return;
+	}
+
+	printDefaultHelp(currentNode->nodeRight, level+1, false);
+	
+	for (int i = 0; i < level - 1; i++) {
+		cout.width(4);
+		cout << "  |";
+	}
+
+	if (level > 0) {
+		cout.width(4);
+		cout.fill('-');
+		cout << "+";
+		cout.fill(' ');
+	}
+	cout <<"("<< currentNode->getData()<<")"<< endl;
+
+	printDefaultHelp(currentNode->nodeLeft, level + 1, true);
+}
+
+template <typename T>
+int Tree<T>::countNode()const {
+	return countNodeHelp(root);
+}
+
+template <typename T>
+int Tree<T>::countNodeHelp(Node<T> *currentNode)const {
+	
+	if (currentNode == NULL) {
+		return 0;
+	}
+	
+	return 1 + countNodeHelp(currentNode->nodeRight) + countNodeHelp(currentNode->nodeLeft);
 
 }
 
