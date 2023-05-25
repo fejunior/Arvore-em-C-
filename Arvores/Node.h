ac7bd29ff4a6e19c2a27f50a13225d69f1f22419
Fernando Gomes de Sousa Junior <fejunior5@hotmail.com>
2023-05-25 06:45:07 -0300

    error in factorBalance

--- Arvores/Node.h
+++ Arvores/Node.h
@@ -1,17 +1,26 @@
 #pragma once
-template <typename T> class Tree;//Declaração da classe Arvore
+#include <iostream>
+
+using namespace std;
 
 template <typename T>
 class Node
 {
-	friend class Tree<T>;//Permite a classe Tree acessar os atributos privados da classe Node 
 	private:
 		T data;
 		Node<T> *nodeLeft;
 		Node<T> *nodeRight;
+		int subtreeHeight(Node<T>*);
 	public:
 		Node(const T&);
 		T getData();
+		void setLeft(Node<T>*);
+		void setRight(Node<T>*);
+		Node<T>* getLeft();
+		Node<T>* getRight();
+		int balanceFactor();
+		int nodeHeight();
+		
 };
 
 template <typename T>
@@ -26,3 +35,47 @@ T Node<T>::getData() {
 	return data;
 }
 
+template <typename T>
+void Node<T>::setLeft(Node<T>* left) {
+	nodeLeft= left;
+}
+
+template <typename T>
+void Node<T>::setRight(Node<T>* right) {
+	nodeRight = right;
+}
+
+template <typename T>
+Node<T>* Node<T>::getLeft() {
+	return nodeLeft;
+}
+
+template <typename T>
+Node<T>* Node<T>::getRight() {
+	return nodeRight;
+}
+
+template <typename T>
+int Node<T>::balanceFactor() {
+	
+	return subtreeHeight(nodeLeft) - subtreeHeight(nodeRight);
+}
+
+template <typename T>
+int Node<T>::nodeHeight() {
+	
+	return subtreeHeight(this) ;
+}
+
+template <typename T>
+int Node<T>::subtreeHeight(Node<T>* node) {
+
+	if (node == nullptr) {
+		return -1;
+	}
+
+	int heightLeft  = subtreeHeight(node->getLeft());
+	int heigthRight = subtreeHeight(node->getRight());
+
+	return max(heightLeft, heigthRight)+1;
+}
--- Arvores/Tree.h
+++ Arvores/Tree.h
@@ -1,20 +1,24 @@
 #pragma once
 #include "Node.h"
-#include <iostream>
 
-using namespace std;
 
 template <typename T> 
 class Tree
 {
 	private:
-		Node<T>* root;
-		void insertNodeHelp(Node<T>**, const T&);//Auxiliar para inserir um noh
+		Node<T> *root;
+		Node<T>* insertNodeHelp(Node<T>*, const T&);//Auxiliar para inserir um noh
 		void printPreOrderHelp(Node<T>*)const;
 		void printInOrderHelp(Node<T>*)const;
 		void printPosOrderHelp(Node<T>*)const;
 		void printDefaultHelp(Node<T>*, int, bool)const;
 		int countNodeHelp(Node<T>*)const;
+		bool treeIsEmpty()const;
+		void checkBalanceHelper(Node<T>*)const;
+		Node<T>* simpleLeftRotation(Node<T>*)const;
+		Node<T>* doubleRightLeftRotation(Node<T>*)const;
+		Node<T>* simpleRightRotation(Node<T>*)const;
+		Node<T>* doubleLeftRightRotation(Node<T>*)const;
 	public:
 		Tree();
 		void insertNode(const T&);//Inserir um noh
@@ -23,6 +27,8 @@ class Tree
 		void printPosOrder()const;
 		void printDefault()const;
 		int countNode()const;
+		void checkBalance()const;
+		
 
 };
 
@@ -32,26 +38,28 @@ Tree<T>::Tree() {
 }
 
 template <typename T>
-void Tree<T>::insertNode(const T &value) {
-	insertNodeHelp(&root, value);
+void Tree<T>::insertNode(const T& value) {
+	root = insertNodeHelp(root, value);
+	checkBalanceHelper(root);
 }
 
 template <typename T>
-void Tree<T>::insertNodeHelp(Node<T> **currentNode, const T &value) {
-	if (*currentNode == nullptr) {
-		*currentNode = new Node<T>(value);
+Node<T>* Tree<T>::insertNodeHelp(Node<T>* currentNode, const T& value) {
+	if (currentNode == nullptr) {
+		return new Node<T>(value);
+	}
+
+	if (value < currentNode->getData()) {
+		currentNode->setLeft(insertNodeHelp(currentNode->getLeft(), value));
+	}
+	else if (value > currentNode->getData()) {
+		currentNode->setRight(insertNodeHelp(currentNode->getRight(), value));
 	}
 	else {
-		if (value < (*currentNode)->data) {
-			insertNodeHelp(&((*currentNode)->nodeLeft), value);
-		}
-		else if (value > (*currentNode)->data) {
-			insertNodeHelp(&((*currentNode)->nodeRight), value);
-		}
-		else {
-			cout << "Ops! "<<value << " Duplicado!" << endl;
-		}
+		cout << "Ops! " << value << " Duplicado!" << endl;
 	}
+
+	return currentNode;
 }
 
 template <typename T>
@@ -64,8 +72,8 @@ void Tree<T>::printPreOrderHelp(Node<T> *currentNode)const {
 	//Imprime em pre-ordem
 	if (currentNode != nullptr) {
 		cout <<"("<< currentNode->getData();
-		printPreOrderHelp(currentNode->nodeLeft);
-		printPreOrderHelp(currentNode->nodeRight);
+		printPreOrderHelp(currentNode->getLeft());
+		printPreOrderHelp(currentNode->getRight());
 		cout << ")";
 	}
 	else {
@@ -84,9 +92,9 @@ void Tree<T>::printInOrderHelp(Node<T> *currentNode)const {
 	//Imprime em ordem
 	if (currentNode != nullptr) {
 		cout << "(";
-		printInOrderHelp(currentNode->nodeLeft);
+		printInOrderHelp(currentNode->getLeft());
 		cout << currentNode->getData();
-		printInOrderHelp(currentNode->nodeRight);
+		printInOrderHelp(currentNode->getRight());
 		cout << ")";
 	}
 	else {
@@ -104,8 +112,8 @@ void Tree<T>::printPosOrderHelp(Node<T> *currentNode)const {
 	//Imprime em pos-ordem
 	if (currentNode != nullptr) {
 		cout << "(";
-		printPosOrderHelp(currentNode->nodeLeft);
-		printPosOrderHelp(currentNode->nodeRight);
+		printPosOrderHelp(currentNode->getLeft());
+		printPosOrderHelp(currentNode->getRight());
 		cout << currentNode->getData();
 		cout << ")";
 	}
@@ -123,11 +131,11 @@ template <typename T>
 void Tree<T>::printDefaultHelp(Node<T> *currentNode, int level, bool left)const {
 	//Imprime em arvore na horizontal
 	
-	if (currentNode == NULL) {
+	if (currentNode == nullptr) {
 		return;
 	}
 
-	printDefaultHelp(currentNode->nodeRight, level+1, false);
+	printDefaultHelp(currentNode->getRight(), level + 1, false);
 	
 	for (int i = 0; i < level - 1; i++) {
 		cout.width(4);
@@ -142,7 +150,7 @@ void Tree<T>::printDefaultHelp(Node<T> *currentNode, int level, bool left)const 
 	}
 	cout <<"("<< currentNode->getData()<<")"<< endl;
 
-	printDefaultHelp(currentNode->nodeLeft, level + 1, true);
+	printDefaultHelp(currentNode->getLeft(), level + 1, true);
 }
 
 template <typename T>
@@ -153,11 +161,81 @@ int Tree<T>::countNode()const {
 template <typename T>
 int Tree<T>::countNodeHelp(Node<T> *currentNode)const {
 	
-	if (currentNode == NULL) {
+	if (currentNode == nullptr) {
 		return 0;
 	}
 	
-	return 1 + countNodeHelp(currentNode->nodeRight) + countNodeHelp(currentNode->nodeLeft);
+	return 1 + countNodeHelp(currentNode->getRight()) + countNodeHelp(currentNode->getLeft());
 
 }
 
+template <typename T>
+bool Tree<T>::treeIsEmpty()const {
+	return root == nullptr;
+}
+
+template <typename T>
+void Tree<T>::checkBalanceHelper(Node<T>* node)const {
+	int factorBalance = node->balanceFactor();
+
+	if (factorBalance > 1) {
+		//Peso maior para arvore a Direita 
+		if (node->getRight()->balanceFactor() >= 0) {
+			//Peso maior continua a direita 
+		  node = simpleLeftRotation(node);
+		}
+		else {
+			//Peso maior eh na esquerda
+			node = doubleRightLeftRotation(node);
+		}
+	}
+	else if (factorBalance < 0) {
+		//Peso maior para a subarvore a esquerda
+		if (node->getLeft()->balanceFactor() >= 0) {
+			//Peso maior continua a esquerda 
+			node = simpleRightRotation(node);
+		}
+		else {
+			//Peso maior eh na direita
+			node = doubleLeftRightRotation(node);
+		}
+	}
+
+	if (node->getLeft()) { checkBalanceHelper(node->getLeft()); };
+	if (node->getRight()) { checkBalanceHelper(node->getRight()); };
+
+}
+
+template <typename T>
+Node<T> * Tree<T>::simpleRightRotation(Node<T>* node)const {
+	Node<T>* newRoot = node->getLeft();
+	node->setLeft(newRoot->getRight());
+	newRoot->setRight(node);
+	
+	return newRoot;
+}
+
+template <typename T>
+Node<T> * Tree<T>::simpleLeftRotation(Node<T>* node)const {
+	Node<T>* newRoot = node->getRight();
+	node->setRight(newRoot->getLeft());
+	newRoot->setLeft(node);
+	
+	return newRoot;
+}
+
+template <typename T>
+Node<T>* Tree<T>::doubleLeftRightRotation(Node<T>* node)const {
+	
+		node->setLeft(simpleLeftRotation(node->getLeft()));
+
+		return simpleRightRotation(node);
+}
+
+template <typename T>
+Node<T>* Tree<T>::doubleRightLeftRotation(Node<T>* node)const {
+
+	node->setRight(simpleRightRotation(node->getRight()));
+
+	return simpleLeftRotation(node);
+}
